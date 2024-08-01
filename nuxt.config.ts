a6ca8b397c8f6ce945e83bfd2aca634807a4460b
theophileiikv <stheophile31@gmail.com>
2024-08-01 18:16:57 +0300

    x

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxt/image", "@nuxt/ui"],
   colorMode: {
-    preference: 'light'
+    preference: 'light',
+    fallback: 'light'
   }
 })
